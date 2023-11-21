25234562d96854b10aeb969199a337a76279a3c0
qwynbleid <101746597+qwynbleid@users.noreply.github.com>
2023-11-21 23:08:25 +0200

    Update ProgramCode.java

--- ProgramCode.java
+++ ProgramCode.java
@@ -1,5 +1,22 @@
-class HelloWorld {
-    public static void main(String[] args) {
+/**
+ * The HelloWorld class prints "Hello World!" to the console.
+ */
+public final class HelloWorld {
+
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private HelloWorld() {
+        // Utility classes should not have public or default constructors
+    }
+
+    /**
+     * The main method that prints "Hello World!" to the console.
+     *
+     * @param args The command line arguments.
+     */
+    public static void main(final String[] args) {
+        // Parameter args should be final
         System.out.println("Hello World!");
     }
 }
